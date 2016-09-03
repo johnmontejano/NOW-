ed53141163193f9cc1e4c26a825bb490bb094218
johnmontewhat <us4john@gmail.com>
2016-09-02 21:39:20 -0700

    Going to start back tomorrow (I'm out)

--- NOW/ViewController.swift
+++ NOW/ViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableViewController: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewController.dataSource = self
+        tableViewController.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hi"
+        return cell
+    }
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        prepareForSegue(<#T##segue: UIStoryboardSegue##UIStoryboardSegue#>, sender: <#T##AnyObject?#>)
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
